cef86187f39f68f5cb82069d9562132acda2cd4d
Максим Богинич <maksimboginic@Maksims-MacBook-Pro.local>
2021-04-23 01:19:00 +0300

    Has done

--- scripts/twitter/tweet_utils.py
+++ scripts/twitter/tweet_utils.py
@@ -2,7 +2,7 @@ import time
 from twitter.LoginPage import SearchHelper
 
 def tweeted(browser, username, password, title):
-    try:
+
         twitter_login_page = SearchHelper(browser)
         twitter_login_page.go_to_site()
         twitter_login_page.enter_username(username)
@@ -12,7 +12,4 @@ def tweeted(browser, username, password, title):
         time.sleep(5)
         twitter_login_page.click_on_the_tweet_button()
         time.sleep(5)
-    except:
-       print('Something happened with posted of twitter')
-       twitter_login_page.quit()
 
